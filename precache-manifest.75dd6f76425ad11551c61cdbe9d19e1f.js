accc4d7fd4afb1603c013cabda2035a64bbf949a
Shanu Goyanka <sgoyanka@techarcers.io>
2020-12-14 21:40:46 +0530

    Updates

---
+++ precache-manifest.75dd6f76425ad11551c61cdbe9d19e1f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5662e09daafd72c3508db514fee00f7",
+    "url": "/hyfi-ui/index.html"
+  },
+  {
+    "revision": "f7514215f3cf50d1d270",
+    "url": "/hyfi-ui/static/css/main.9e8854b3.chunk.css"
+  },
+  {
+    "revision": "914e608ddcf54fb69a2c",
+    "url": "/hyfi-ui/static/js/0.47fd55f5.chunk.js"
+  },
+  {
+    "revision": "14202f949bc227e53ed6",
+    "url": "/hyfi-ui/static/js/10.290179d5.chunk.js"
+  },
+  {
+    "revision": "394c72d1903d35883c7f",
+    "url": "/hyfi-ui/static/js/11.37e10337.chunk.js"
+  },
+  {
+    "revision": "cd846d4690383206d7a8",
+    "url": "/hyfi-ui/static/js/3.ee0f0f2a.chunk.js"
+  },
+  {
+    "revision": "15723851b11509ef1b9a8dbd2a0ef589",
+    "url": "/hyfi-ui/static/js/3.ee0f0f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "879306bc7bfc3b71ab17",
+    "url": "/hyfi-ui/static/js/4.0a77c028.chunk.js"
+  },
+  {
+    "revision": "9fbe265e2436f8fd7620",
+    "url": "/hyfi-ui/static/js/5.901a3db4.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/hyfi-ui/static/js/5.901a3db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e14be594ba0312643aa",
+    "url": "/hyfi-ui/static/js/6.848658a2.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/hyfi-ui/static/js/6.848658a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56405646415c899d19bc",
+    "url": "/hyfi-ui/static/js/7.edb7fb7c.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/hyfi-ui/static/js/7.edb7fb7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeffb963838191e37bb3",
+    "url": "/hyfi-ui/static/js/8.d8bd13a4.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/hyfi-ui/static/js/8.d8bd13a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e048bfd864c2f15a62da",
+    "url": "/hyfi-ui/static/js/9.5f7d0c8c.chunk.js"
+  },
+  {
+    "revision": "f7514215f3cf50d1d270",
+    "url": "/hyfi-ui/static/js/main.328e9785.chunk.js"
+  },
+  {
+    "revision": "53598cd91aeb88570496",
+    "url": "/hyfi-ui/static/js/runtime-main.0cee1af3.js"
+  },
+  {
+    "revision": "4765af3327923874c76442c2ff86b363",
+    "url": "/hyfi-ui/static/media/chef.4765af33.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/hyfi-ui/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/hyfi-ui/static/media/wallet-connect.37f2bc6e.svg"
+  },
+  {
+    "revision": "2bb1eb4dba1c03a327571ed5359ecae4",
+    "url": "/hyfi-ui/static/media/wpp.2bb1eb4d.png"
+  }
+]);
---
+++ static/js/11.37e10337.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonphyfi-ui"]=this["webpackJsonphyfi-ui"]||[]).push([[11],{530:function(i,h){}}]);
+//# sourceMappingURL=11.37e10337.chunk.js.map
